908a9b4447bb12f9aec1de0b2f9839198d480e0b
Kevin Winkler <kevin.winkler@cn-consult.eu>
2019-09-25 16:52:46 +0200

    Add CLI options

---
+++ README.md
@@ -0,0 +1,8 @@
+# Game of Life
+
+The Game of Life algorithm implemented as a PHP CLI program.
+
+## Useage
+
+Run gol.php to run the program with its default parameter.
+use -h to print all available comandline options.
--- src/gol.php
+++ src/gol.php
@@ -4,13 +4,74 @@ use GOL\Boards\Board;
 use GOL\Boards\BoardGlider;
 use GOL\Boards\BoardRandom;
 use GOL\Rule;
+use Ulrichsg\Getopt;
 
 require_once "include.php";
+require_once "ulrichsg/getopt.php";
 
-$field = new BoardGlider(10,10);
-$rule = new Rule([3], [2, 3]);
-$maxIteration= 21;
+$maxIteration = 21;
 $version = "1.0";
+$width = 10;
+$height = 10;
+
+$field = null;
+$rule = new Rule([3], [2, 3]);
+
+$getOpt = new Getopt(
+    [
+        ['h', 'help', Getopt::NO_ARGUMENT, "Prints this page"],
+        ['v', 'version', Getopt::NO_ARGUMENT, "Prints the version at program start"],
+
+        [null, 'width', Getopt::REQUIRED_ARGUMENT, "Width of the board"],
+        [null, 'height', Getopt::REQUIRED_ARGUMENT, "Height of the board"],
+        ['m', 'maxIteration', Getopt::REQUIRED_ARGUMENT, "Maximum number of iteration"],
+        [null, 'startRandom', Getopt::NO_ARGUMENT, "Start with random values"],
+        [null, 'startGlider', Getopt::NO_ARGUMENT, "Start with a glider in the upper left corner"]
+    ]);
+
+$getOpt->parse();
+
+if ($getOpt->getOption('h'))
+{
+    $getOpt->showHelp();
+    die;
+}
+if ($getOpt->getOption('v'))
+{
+    echo "Game of Life algorithm in php.\n" .
+        "Version $version \n";
+    die;
+}
+
+if ($getOpt->getOption('width'))
+{
+    $width = intval($getOpt->getOption('width'));
+    if ($width == 0)
+        $width = 10;
+}
+if ($getOpt->getOption('height'))
+{
+    $height = intval($getOpt->getOption('height'));
+    if ($height == 0)
+        $height = 10;
+}
+if ($getOpt->getOption('m'))
+{
+    $maxIteration = intval($getOpt->getOption('m'));
+}
+
+if ($getOpt->getOption("startRandom"))
+{
+    $field = new BoardRandom($width, $height);
+}
+else if ($getOpt->getOption("startGlider"))
+{
+    $field = new BoardGlider($width, $height);
+}
+else
+{
+    $field = new BoardGlider($width, $height);
+}
 
 function nextGeneration(Board &$_board, Rule &$_rule)
 {
