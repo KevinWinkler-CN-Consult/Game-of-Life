70e219387fae8a60f99814362c17bdc04ff00c67
Kevin Winkler <kevin.winkler@cn-consult.eu>
2019-10-25 14:56:46 +0200

    Add video output

---
+++ src/output/video.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace GOL\Output;
+
+use GOL\Boards\Board;
+use Ulrichsg\Getopt;
+
+/**
+ * Saves the Board as a video.
+ *
+ * Use write() to write the Board data to the output.
+ * and flush() to write it on the disc.
+ */
+class Video extends Output
+{
+    private $buffer = [];
+
+    /**
+     * Writes the current board to the Output.
+     *
+     * @param Board $_board Board to output.
+     */
+    public function write(Board $_board): void
+    {
+        $this->buffer[] = $_board->getGrid();
+    }
+
+    /**
+     * Writes the data to the console.
+     */
+    public function flush(): void
+    {
+        if(!is_dir("out/"))
+            mkdir("out/", 0755);
+
+        foreach ($this->buffer as $index => $board)
+        {
+            $with = count($board);
+            $height = count($board[0]);
+
+            $image = imagecreate($with, $height);
+            imagecolorallocate($image, 0, 0, 0);
+            $white = imagecolorallocate($image, 255, 255, 255);
+
+            for ($y = 0; $y < $height; $y++)
+            {
+                for ($x = 0; $x < $with; $x++)
+                {
+                    if ($board[$x][$y] == 1)
+                        imagesetpixel($image, $x, $y, $white);
+                }
+            }
+
+            imagepng($image, "out/" . sprintf("img-%03d",$index) . ".png");
+        }
+
+        exec( "ffmpeg -i out/img-%03d.png out/video.avi" );
+        exec( "rm out/img*" );
+
+        $this->buffer = [];
+    }
+
+    /**
+     * Returns the description of the Output.
+     * @return string description.
+     */
+    public function description(): string
+    {
+        return "Outputs the Board as a video.";
+    }
+}
