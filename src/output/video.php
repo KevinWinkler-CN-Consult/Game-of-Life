934f56cdcc4f060506dc621b3132f524664e818d
Kevin Winkler <kevin.winkler@cn-consult.eu>
2019-10-28 11:37:37 +0100

    Add optional video parameters

--- src/output/video.php
+++ src/output/video.php
@@ -14,6 +14,10 @@ use Ulrichsg\Getopt;
 class Video extends Output
 {
     private $buffer = [];
+    private $fps = 30.0;
+    private $cellColor = [];
+    private $backgroundColor = [];
+    private $cellSize = 1;
 
     /**
      * Writes the current board to the Output.
@@ -38,26 +42,90 @@ class Video extends Output
             $with = count($board);
             $height = count($board[0]);
 
-            $image = imagecreate($with, $height);
-            imagecolorallocate($image, 0, 0, 0);
-            $white = imagecolorallocate($image, 255, 255, 255);
+            $image = imagecreate($with * $this->cellSize, $height * $this->cellSize);
+            imagecolorallocate($image, $this->backgroundColor[0], $this->backgroundColor[1], $this->backgroundColor[2]);
+            $cellColor = imagecolorallocate($image, $this->cellColor[0], $this->cellColor[1], $this->cellColor[2]);
 
             for ($y = 0; $y < $height; $y++)
             {
-                for ($x = 0; $x < $with; $x++)
+                for ($sy = 0; $sy < $this->cellSize; $sy++)
                 {
-                    if ($board[$x][$y] == 1)
-                        imagesetpixel($image, $x, $y, $white);
+                    for ($x = 0; $x < $with; $x++)
+                    {
+                        if ($board[$x][$y] == 1)
+                            for ($sx = 0; $sx < $this->cellSize; $sx++)
+                            {
+                                imagesetpixel($image, $x * $this->cellSize + $sx, $y * $this->cellSize + $sy, $cellColor);
+                            }
+                    }
                 }
             }
 
             imagepng($image, "out/" . sprintf("img-%03d",$index) . ".png");
         }
 
-        exec( "ffmpeg -i out/img-%03d.png out/video.avi" );
+        exec("ffmpeg -framerate " . $this->fps . " -i out/img-%03d.png out/video.avi");
         exec( "rm out/img*" );
 
         $this->buffer = [];
+    }
+
+    /**
+     * Checks for optional parameters.
+     * @param Getopt $_getopt Option manager to check for optional parameters.
+     */
+    public function checkParamerters(Getopt $_getopt): void
+    {
+        $this->fps = $_getopt->getOption("videoFPS");
+        if ($this->fps <= 0.0)
+            $this->fps = 30.0;
+
+        $this->cellSize = $_getopt->getOption("videoCellSize");
+        if ($this->cellSize <= 0)
+            $this->cellSize = 1;
+
+        foreach (explode(",", $_getopt->getOption("videoCellColor")) as $item => $value)
+        {
+            if ($value == "")
+                break;
+
+            $this->cellColor[$item] = $value;
+        }
+        if (empty($this->cellColor))
+        {
+            $this->cellColor[0] = 255;
+            $this->cellColor[1] = 255;
+            $this->cellColor[2] = 255;
+        }
+
+        foreach (explode(",", $_getopt->getOption("videoBackgroundColor")) as $item => $value)
+        {
+            if ($value == "")
+                break;
+
+            $this->backgroundColor[$item] = $value;
+        }
+        if (empty($this->backgroundColor))
+        {
+            $this->backgroundColor[0] = 0;
+            $this->backgroundColor[1] = 0;
+            $this->backgroundColor[2] = 0;
+        }
+    }
+
+    /**
+     * Register all optional parameters the Output.
+     * @param Getopt $_getopt Option manager to add the options
+     */
+    public function register(Getopt $_getopt): void
+    {
+        $_getopt->addOptions(
+            [
+                [null, "videoFPS", Getopt::REQUIRED_ARGUMENT, "Sets the framerate of the video."],
+                [null, "videoCellColor", Getopt::REQUIRED_ARGUMENT, "Sets the color of living cells. 'r,g,b' 0-255."],
+                [null, "videoBackgroundColor", Getopt::REQUIRED_ARGUMENT, "Sets the background color. 'r,g,b' 0-255."],
+                [null, "videoCellSize", Getopt::REQUIRED_ARGUMENT, "Sets the size of the cells in pixel."]
+            ]);
     }
 
     /**
