ba143dae00aaedfd65a971bc41d1ba02df68e9fb
Kevin Winkler <kevin.winkler@cn-consult.eu>
2019-11-18 17:24:29 +0100

    Add phpunit test config

---
+++ test/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /**
+     * @test
+     */
+    public function PHPUnitWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
