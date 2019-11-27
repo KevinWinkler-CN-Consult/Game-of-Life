1e057829204d60247761c321cf83218a47fdfa7c
Kevin Winkler <kevin.winkler@cn-consult.eu>
2019-11-27 10:31:36 +0100

    Changed reworked RLE file parsing

--- src/Input/RLE.php
+++ src/Input/RLE.php
@@ -46,38 +46,16 @@ class RLE extends Input
     public function prepareBoard(Board &$_board, Getopt $_getopt): void
     {
         $filePath = $_getopt->getOption("rleFile");
+
         if ($filePath == null)
         {
             echo "Option --rleFile must be set!\n";
             return;
         }
-        $sizeX = 0;
-        $sizeY = 0;
-        $posX = 0;
-        $posY = 0;
 
         $fileHandle = fopen($filePath, 'r');
 
-        //read header
-        while ($line = fgets($fileHandle))
-        {
-            if (substr($line, 0, 1) == '#')
-                continue;
-
-            $temp = trim($line);
-            $content = explode(',', $temp);
-
-            $sizeX = explode('=', $content[0])[1];
-            $sizeY = explode('=', $content[1])[1];
-
-            break;
-        }
-
-        if ($sizeX > $_board->width() || $sizeY > $_board->height())
-            echo "pattern to big for given field!";
-
-        $offsetX = floor($_board->width() / 2 - $sizeX / 2.0);
-        $offsetY = floor($_board->height() / 2 - $sizeY / 2.0);
+        list($sizeX, $sizeY) = $this->readHeader($fileHandle);
 
         if ($_getopt->getOption("rlePosition"))
         {
@@ -85,51 +63,50 @@ class RLE extends Input
             $offsetX = intval($arg[0]);
             $offsetY = intval($arg[1]);
         }
+        else
+        {
+            $offsetX = floor($_board->width() / 2 - $sizeX / 2.0);
+            $offsetY = floor($_board->height() / 2 - $sizeY / 2.0);
+        }
+
+        if ($offsetX + $sizeX <= $_board->width() || $offsetY + $sizeY <= $_board->height())
+            echo "Pattern to big or offset to high. Pattern might be cropped.";
+        if ($offsetX <= 0 || $offsetY <= 0)
+            echo "Negative offset! Pattern might be cropped.";
 
         //read data
-        $run = true;
-        while ($line = fgets($fileHandle))
+        $posX = 0;
+        $posY = 0;
+        $buffer = "";
+        while ($char = fgetc($fileHandle))
         {
-            $numberStart = -1;
-
-            //for each char
-            for ($i = 0; $i < strlen($line) && $run; $i++)
+            if (is_numeric($char))
             {
-                $char = substr($line, $i, 1);
-
-                if (is_numeric($char))
-                {
-                    if ($numberStart == -1)
-                        $numberStart = $i;
-                    continue;
-                }
-
-                $runCount = 1;
-                if ($numberStart != -1)
-                    $runCount = intval(substr($line, $numberStart, $i + 1 - $numberStart));
-
-                switch ($char)
-                {
-                    case '!':
-
-                        $run = false;
-                        break;
-                    case '$':
-                        $posX = 0;
-                        $posY += $runCount;
-                        break;
-                    case 'o':
-                        for ($x = $posX; $x < ($posX + $runCount); $x++)
-                            $_board->setCell($x + $offsetX, $posY + $offsetY, 1);
-
-                        $posX += $runCount;
-                        break;
-                    case 'b':
-                        $posX += $runCount;
-                }
-                $numberStart = -1;
-
+                $buffer .= $char;
+                continue;
             }
+
+            $runCount = intval($buffer, 10) != 0 ? intval($buffer, 10) : 1;
+
+            switch ($char)
+            {
+                case 'o':
+                    for ($x = $posX; $x < ($posX + $runCount); $x++)
+                        $_board->setCell($x + $offsetX, $posY + $offsetY, 1);
+                    $posX += $runCount;
+                    break;
+                case 'b':
+                    $posX += $runCount;
+                    break;
+                case '$':
+                    $posX = 0;
+                    $posY += $runCount;
+                    break;
+            }
+            $buffer = "";
+
+            if ($char == '!')
+                break;
         }
         fclose($fileHandle);
     }
@@ -156,5 +133,30 @@ class RLE extends Input
     public function description(): string
     {
         return "creates a board with a specific RLE pattern";
+    }
+
+    /**
+     * Reads the header of the RLE file and returns the size of the pattern.
+     * @param $fileHandle resource opened RLE file.
+     * @return array [size x,size y].
+     */
+    private function readHeader($fileHandle): array
+    {
+        $sizeX = 0;
+        $sizeY = 0;
+        while ($line = fgets($fileHandle))
+        {
+            if (substr($line, 0, 1) == '#')
+                continue;
+
+            $temp = trim($line);
+            $content = explode(',', $temp);
+
+            $sizeX = explode('=', $content[0])[1];
+            $sizeY = explode('=', $content[1])[1];
+
+            break;
+        }
+        return array($sizeX, $sizeY);
     }
 }
