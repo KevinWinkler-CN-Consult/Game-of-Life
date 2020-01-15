6b6f686db39c5420cd7cbfa23b57781248dcef47
Kevin Winkler <kevin.winkler@cn-consult.eu>
2020-01-15 11:23:29 +0100

    Add Standard Game of Life rule

---
+++ src/Rules/StandardRule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GOL\Rules;
+
+use GOL\Boards\Field;
+
+class StandardRule extends Rule
+{
+    public function apply(Field $field): bool
+    {
+        $survive = [0, 0, 1, 1, 0, 0, 0, 0, 0];
+        $born = [0, 0, 0, 1, 0, 0, 0, 0, 0];
+        $numberOfLivingNeighbors = $field->numberOfLivingNeighbors();
+
+        return $field->value() ? $survive[$numberOfLivingNeighbors] : $born[$numberOfLivingNeighbors];
+    }
+}
---
+++ test/GOL/Rules/StandardRuleTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Rules;
+
+use GOL\Boards\Board;
+use GOL\Rules\StandardRule;
+use PHPUnit\Framework\TestCase;
+
+class StandardRuleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function noLivingNeighboursYieldDeadCell()
+    {
+        $board = new Board(4, 4);
+        $rule = new StandardRule();
+
+        $newValue = $rule->apply($board->getCell(1, 1));
+        $this->assertFalse($newValue);
+    }
+
+    /**
+     * @test
+     */
+    public function noLivingNeighboursYieldDeadCell2()
+    {
+        $board = new Board(4, 4);
+        $rule = new StandardRule();
+
+        $board->setCell(1, 1, true);
+
+        $newValue = $rule->apply($board->getCell(1, 1));
+        $this->assertFalse($newValue);
+    }
+
+    /**
+     * @test
+     */
+    public function surviveOn2Neighbours()
+    {
+        $board = new Board(4, 4);
+        $rule = new StandardRule();
+
+        $board->setCell(1, 1, true);
+
+        $board->setCell(0, 0, true);
+        $board->setCell(0, 1, true);
+
+        $newValue = $rule->apply($board->getCell(1, 1));
+        $this->assertTrue($newValue);
+    }
+
+    /**
+     * @test
+     */
+    public function surviveOn3Neighbours()
+    {
+        $board = new Board(4, 4);
+        $rule = new StandardRule();
+
+        $board->setCell(1, 1, true);
+
+        $board->setCell(0, 0, true);
+        $board->setCell(0, 1, true);
+        $board->setCell(1, 0, true);
+
+        $newValue = $rule->apply($board->getCell(1, 1));
+        $this->assertTrue($newValue);
+    }
+
+    /**
+     * @test
+     */
+    public function birthOn3Neighbours()
+    {
+        $board = new Board(4, 4);
+        $rule = new StandardRule();
+
+        $board->setCell(0, 0, true);
+        $board->setCell(0, 1, true);
+        $board->setCell(1, 0, true);
+
+        $newValue = $rule->apply($board->getCell(1, 1));
+        $this->assertTrue($newValue);
+    }
+}
